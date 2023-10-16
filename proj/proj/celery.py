6be921e31ea7fa50a5eddb2747c124eeccb4e9a0
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-10-16 20:02:56 +0900

    [UPDATE] 오타 수정

--- proj/proj/celery.py
+++ proj/proj/celery.py
@@ -2,14 +2,14 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
-app = Celery('config')
+app = Celery('proj')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
